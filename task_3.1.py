731bc07a680e99ed6b0be3b3bed34facd9bc8398
Your Name <you@example.com>
2021-02-25 20:01:50 +0700

    Исправлены задания семинара 3

--- task_3.1.py
+++ task_3.1.py
@@ -4,11 +4,9 @@ data = {}
 with open(readfile, 'r', encoding='utf-8') as file1:
     for line in file1:
         read_file = json.loads(line)
-        for item in read_file:
-            if "description" in read_file:
-                data[read_file['label']['value']] = read_file['description']['value']
-            else:
-                data[read_file['label']['value']] = "None"
-    print(data)
+        if "description" in read_file:
+            data[read_file['label']['value']] = read_file['description']['value']
+        else:
+            data[read_file['label']['value']] = None
 with open("json1.json", 'w', encoding='utf-8') as file2:
     json.dump(data, file2, ensure_ascii=False)
