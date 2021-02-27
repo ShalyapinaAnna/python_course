81509b734ec7737cda90d6f8ae99e2715fae2539
Your Name <you@example.com>
2021-02-27 16:06:53 +0700

    Исправлены задания семинара 4

--- task_4.1.py
+++ task_4.1.py
@@ -1,5 +1,4 @@
 import xml.etree.ElementTree as etree
-text = ''
 with open('xml1.txt', 'w', encoding='utf-8') as f:
     tree = etree.parse("annot.opcorpora.no_ambig.xml")
     root = tree.getroot()
--- task_4.3.py
+++ task_4.3.py
@@ -4,12 +4,7 @@ verb = 0
 tree = etree.parse("annot.opcorpora.no_ambig.xml")
 root = tree.getroot()
 for token in root.iter('token'):
-    if token.get('text') == 'может':
-        if token[0][0][0][0].get('v') == 'CONJ':
-            conj += 1
-        elif token[0][0][0][0].get('v') == 'VERB':
-            verb += 1
-    elif token.get('text') == 'Может':
+    if token.get('text') == 'может' or token.get('text') == 'Может':
         if token[0][0][0][0].get('v') == 'CONJ':
             conj += 1
         elif token[0][0][0][0].get('v') == 'VERB':
